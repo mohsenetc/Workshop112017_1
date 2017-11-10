1976c5118996c53c19c8bc9ed0b1b6f4f4284746
mohsen ahmadian <mohsen.etc@gmail.com>
2017-11-11 00:25:36 +0100

    update mohsen version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Bikram from './Bikram'
-
+import Mohsen from './mohsen'
 class App extends Component {
   render() {
     return (
@@ -14,9 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      <p> Chansong
+      <p> Mohsen
       </p>
         <Bikram/>
+        <Mohsen/>
       </div>
     );
   }
---
+++ src/mohsen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class mohsen extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello iam mohsen :)</h3>
+            </div>
+        );
+    }
+}
+
+export default mohsen;
